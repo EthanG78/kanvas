e4c091650eaccc5d229b5b8200826b9acd65620f
Justen Di Ruscio <justend29@gmail.com>
2022-03-16 17:28:26 -0300

    cleaned

--- src/main/kotlin/brushes/DrawBrush.kt
+++ src/main/kotlin/brushes/DrawBrush.kt
@@ -2,10 +2,7 @@ package brushes
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import marks.ContinuousMark
-import marks.JoiningMark
-import marks.LineMark
-import marks.Mark
+import marks.*
 
 class DrawBrush : BaseBrush() {
     private var width: Float = 3.0F
@@ -15,7 +12,6 @@ class DrawBrush : BaseBrush() {
     }
 
     override fun createMark(color: Color, pos: Offset): Mark {
-        print("creating draw mark")
         //return ContinuousMark(LineMark(color, width))
         return JoiningMark(LineMark(color, pos, width))
     }
