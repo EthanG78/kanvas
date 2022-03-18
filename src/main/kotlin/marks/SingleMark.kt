23dd3fafc8da00ad9fa58639ae43e82dd47bad3a
Justen Di Ruscio <justend29@gmail.com>
2022-03-18 14:33:32 -0300

    single marks don't need to inherit from the prototype mark, Mark already does

--- src/main/kotlin/marks/SingleMark.kt
+++ src/main/kotlin/marks/SingleMark.kt
@@ -3,7 +3,7 @@ package marks
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 
-abstract class SingleMark(protected var color: Color, protected var pos: Offset) : Mark, MarkPrototype {
+abstract class SingleMark(protected var color: Color, protected var pos: Offset) : Mark {
     override fun applyStroke(pos: Offset) {
         if (pos != this.pos) {
             this.pos = pos;
